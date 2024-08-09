f79ca82a4399a663b64501d1af3130f36f5ea8ea
ygnibrahim <thedown172@gmail.com>
2024-08-09 10:04:57 +0300

    Add project files.

---
+++ Caching/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Caching.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memortCache)
+        {
+            _memoryCache = memortCache;
+        }
+        public IActionResult Index()
+        {
+
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+
+            options.AbsoluteExpiration = DateTime.Now.AddSeconds(20);
+
+            //options.SlidingExpiration = TimeSpan.FromSeconds(10);
+
+            options.Priority = CacheItemPriority.High;
+
+            options.RegisterPostEvictionCallback((key, value, reason, state) =>
+            {
+                _memoryCache.Set("callback", $"{key}->{value} => sebep:{reason}");
+            });
+
+
+            _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
+
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+
+            _memoryCache.TryGetValue("zaman", out string zamancache);
+            _memoryCache.TryGetValue("callback", out string callback);
+
+            ViewBag.zaman = zamancache;
+            ViewBag.callback = callback;
+            return View();
+        }
+    }
+}
